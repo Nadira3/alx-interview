34293f117759c9fc9d249bb0b82cc77e49e31af3
Nadira3 <paitanun35@gmail.com>
2024-10-03 06:02:53 +0100

    created direectory and pycodestyle error fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,20 +2,21 @@ def recurse_column(triangle, n):
     if triangle.__len__() == n:
         return triangle
     for i in range(n - 1):
-        triangle.append(recurse_row(triangle, [], 0, n)) # line 5
+        triangle.append(recurse_row(triangle, [], 0, n))
     return triangle
 
-def recurse_row(triangle, new_row,n, i):
+
+def recurse_row(triangle, new_row, n, i):
     old_row = triangle[triangle.__len__() - 1]
-    if n == 0: # line 10 <- line that just executed
+    if n == 0:
         new_row.append(1)
-    elif old_row.__len__() == new_row.__len__(): # line 12 <- current line in the debugger
+    elif old_row.__len__() == new_row.__len__():
         new_row.append(1)
     else:
         new_row.append(old_row[n - 1] + old_row[n])
     if triangle.__len__() == n:
         return new_row
-    return recurse_row(triangle, new_row, n + 1, i) # line 18
+    return recurse_row(triangle, new_row, n + 1, i)
 
 
 def pascal_triangle(n):
@@ -23,4 +24,4 @@ def pascal_triangle(n):
     if n < 1:
         triangle.append([])
         return triangle
-    return recurse_column([[1]], n) # line 26
+    return recurse_column([[1]], n)
