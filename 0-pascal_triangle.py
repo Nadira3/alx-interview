ab7c324622e6e574ce4f09cdc374974f7a543441
Nadira3 <paitanun35@gmail.com>
2024-10-03 05:53:03 +0100

    first attempt submission for task

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,26 @@
+def recurse_column(triangle, n):
+    if triangle.__len__() == n:
+        return triangle
+    for i in range(n - 1):
+        triangle.append(recurse_row(triangle, [], 0, n)) # line 5
+    return triangle
+
+def recurse_row(triangle, new_row,n, i):
+    old_row = triangle[triangle.__len__() - 1]
+    if n == 0: # line 10 <- line that just executed
+        new_row.append(1)
+    elif old_row.__len__() == new_row.__len__(): # line 12 <- current line in the debugger
+        new_row.append(1)
+    else:
+        new_row.append(old_row[n - 1] + old_row[n])
+    if triangle.__len__() == n:
+        return new_row
+    return recurse_row(triangle, new_row, n + 1, i) # line 18
+
+
+def pascal_triangle(n):
+    triangle = []
+    if n < 1:
+        triangle.append([])
+        return triangle
+    return recurse_column([[1]], n) # line 26
