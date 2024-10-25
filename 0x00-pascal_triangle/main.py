128cc4b660dcc6cc5bf44ae96794391386c1c147
Nadira3 <paitanun35@gmail.com>
2024-10-25 04:43:53 +0100

    main file for pascal triangle task

--- 0x00-pascal_triangle/main.py
+++ 0x00-pascal_triangle/main.py
@@ -26,4 +26,6 @@ if __name__ == "__main__":
     print_triangle(pascal_triangle(4))
     print("----------------------------------")
     print_triangle(pascal_triangle(5))
+    print("----------------------------------")
+    print_triangle(pascal_triangle(10))
 
