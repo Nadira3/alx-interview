a067c56a22a81ebd59576116ca9be35bd141deac
Nadira3 <paitanun35@gmail.com>
2024-10-25 05:43:48 +0100

    used signal to handle keyboard interrupt. control-c

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,6 +5,7 @@
 
 import sys
 import re
+import signal
 
 # Define the regex pattern
 substring_a = r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] '
@@ -24,27 +25,28 @@ def print_stats():
     for code in sorted(status_code_counts.keys()):
         print(f"{code}: {status_code_counts[code]}")
 
-
-try:
-    for line in sys.stdin:
-        line = line.strip()
-        match = log_pattern.match(line)
-        if match:
-            ip_address, date, status_code, file_size = match.groups()
-            status_code = int(status_code)
-            file_size = int(file_size)
-
-            total_size += file_size
-
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
-            else:
-                status_code_counts[status_code] = 1
-
-        line_count += 1
-
-        if line_count % 10 == 0:
-            print_stats()
-
-except Exception as err:
+def signal_handler(sig, frame):
     print_stats()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+for line in sys.stdin:
+    line = line.strip()
+    match = log_pattern.match(line)
+    if match:
+        ip_address, date, status_code, file_size = match.groups()
+        status_code = int(status_code)
+        file_size = int(file_size)
+
+        total_size += file_size
+
+        if status_code in status_code_counts:
+            status_code_counts[status_code] += 1
+        else:
+            status_code_counts[status_code] = 1
+
+    line_count += 1
+
+    if line_count % 10 == 0:
+        print_stats()
